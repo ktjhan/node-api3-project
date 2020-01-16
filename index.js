44445c64ed7528764bc475ce6c4a3cba137b7c74
ktjhan <tjhan.kavin@gmail.com>
2020-01-16 14:23:32 -0800

    deleted package.json lock

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./server.js');
 const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
-  console.log('\n* Server Running on http://localhost:' `${port}`);
+  console.log('\n* Server Running on http://localhost: 4000');
 });
